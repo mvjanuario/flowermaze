31248d285d81a7f55657e77818b1daf142251c21
Marcos Januário <mvjanuario@hotmail.com>
2023-02-21 19:22:01 -0300

    Atualizando sprites e fazendo Parallax no Ceu

---
+++ FlowerMaze/Assets/Scripts/SkyParallax.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyParallax : MonoBehaviour
+{
+    private float currentPosition;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPosition = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < 50)
+        {
+            transform.position = new Vector3(currentPosition + parallaxEffect, transform.position.y, transform.position.z);
+            currentPosition = transform.position.x;
+        }
+        else
+        {
+            transform.position = new Vector3(-50, transform.position.y, transform.position.z);
+            currentPosition = transform.position.x;
+        }
+    }
+}
